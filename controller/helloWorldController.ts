791850066e35d531fceb30a5614107f736d94967
Morgan Mcdonald <morgano1312@gmail.com>
2023-09-12 14:58:10 +0100

    Revert "001-Create-Skeleton"

--- controller/helloWorldController.ts
+++
@@ -1,9 +0,0 @@
-import { Application, Request, Response } from "express";
-
-module.exports = function(app: Application){
-
-    app.get('/helloworld', async (req:Request, res: Response)=>{
-         res.render('helloWorld')
-    })
-}
-   
