c655659e7b1bb4a5c16807bb7f41bd5ebbc08ee5
kelvin <kelvin.chan@kainos.com>
2023-09-13 16:03:33 +0100

    Revert "Adjustments"
    This reverts commit 501243261c68f08f393bd7c5dcea37dabf58e510.

--- controller/rolesController.ts
+++ controller/rolesController.ts
@@ -1,27 +1,19 @@
 import { Application, Request, Response } from "express";
 
-const express = require('express')
-const router = express.Router()
+const rolesService = require('../service/rolesService')
 
+module.exports = function(app: Application){
 
-router.get('/', async (req:Request, res:Response) => {     
-    res.render('view-roles') 
-});
-module.exports = router
+    app.get('/job-roles', async (req: Request, res: Response) => {
+        let data = [];
 
+        try{
+            data = await rolesService.getJobRoles()
+        } catch(e){
+            console.error(e);
+        }
+        console.log(data)
+        res.render('view-roles', {Roles: data})
 
-// module.exports = function(app: Application){
-
-//     app.get('/job-roles', async (req: Request, res: Response) => {
-//         let data = [];
-
-//         try{
-//             data = await rolesService.getJobRoles()
-//         } catch(e){
-//             console.error(e);
-//         }
-//         console.log(data)
-//         res.render('view-roles', {Roles: data})
-
-//     })
-// }
+    })
+}
