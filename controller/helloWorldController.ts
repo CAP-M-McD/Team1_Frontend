2142886ec3efe6ff3641bf8579b807905db52aba
leighmcclurg <leigh.mcclurg@kainos.com>
2023-09-13 11:41:03 +0100

    026-adding-branding

--- controller/helloWorldController.ts
+++ controller/helloWorldController.ts
@@ -5,5 +5,9 @@ module.exports = function(app: Application){
     app.get('/helloworld', async (req:Request, res: Response)=>{
          res.render('helloWorld')
     })
+
+    app.get('/index', async (req:Request, res: Response)=>{
+        res.render('index')
+   })
 }
    
