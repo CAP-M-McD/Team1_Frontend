deef493cd5e3f5ffdc9725f02dbec309ed4dafe4
Morgan Mcdonald <morgano1312@gmail.com>
2023-09-20 15:07:03 +0100

    Added HTML adjustments and endpoint

--- service/rolesService.ts
+++ service/rolesService.ts
@@ -2,7 +2,7 @@ import { type Roles } from '../model/Roles'
 const axios = require('axios')
 axios.defaults.baseURL = process.env.API_URL
 
-module.exports.URL = '/job-roles'
+module.exports.URL = '/api/job-roles'
 
 module.exports.getJobRoles = async function (): Promise<Roles[]> {
     try {
